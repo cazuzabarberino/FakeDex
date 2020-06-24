3c6882e2fd0b05053bf6afb662fc22f519167cee
cazuzaneves <cazuzaneves@gmail.com>
2020-06-24 00:39:01 -0300

    pokemon moves v2

--- src/App/components/PokemonMove/index.tsx
+++ src/App/components/PokemonMove/index.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { Container } from "./styles";
+import {
+  Container,
+  MoveInfo,
+  MoveTypeContainer,
+  Header,
+  MoveEffect,
+  MoveTypeAndBadge,
+} from "./styles";
 import Move from "../../models/Move";
 import capitalize from "../../services/Capitalize";
 import { getMoveData } from "../../services/pokeApi";
@@ -14,6 +21,7 @@ interface Props {
 
 export default ({ name, learnLevel, learnMethod }: Props) => {
   const [move, setMove] = useState<Move | null>(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     async function getMove() {
@@ -25,22 +33,69 @@ export default ({ name, learnLevel, learnMethod }: Props) => {
   }, [name]);
 
   const capitalizedName = useMemo(() => capitalize(name), [name]);
+  const effectEntrie = useMemo(
+    () =>
+      move?.effect_entries
+        .find(({ language: { name } }) => name === "en")
+        ?.short_effect.split("$effect_chance%")
+        .join(`${move.effect_chance}%`),
+    [move]
+  );
 
   return (
-    <Container color={move ? colorByType[move.type.name] : undefined}>
-      <div>
+    <Container
+      onClick={(event) => {
+        setOpen((c) => !c);
+        event.stopPropagation();
+      }}
+      open={open}
+      color={move ? colorByType[move.type.name] : undefined}
+    >
+      <Header>
         <h4>
           Lv.{learnLevel} {capitalizedName}
         </h4>
-        {move && <TypeBadge type={move.type.name} />}
-      </div>
+      </Header>
       {move && (
-        <div>
-          {move.effect_entries
-            .find(({ language: { name } }) => name === "en")
-            ?.short_effect.replace("$effect_chance%", `${move.effect_chance}%`)}
-        </div>
+        <>
+          <MoveTypeAndBadge>
+            <TypeBadge type={move.type.name} />
+          </MoveTypeAndBadge>
+          <MoveType type={move.damage_class.name} />
+          {(move.power || move.accuracy) && (
+            <MoveInfo>
+              {move.power && (
+                <div>
+                  <p>power</p>
+                  <h4>{move.power}</h4>
+                </div>
+              )}
+
+              {move.accuracy && (
+                <div>
+                  <p>acc</p>
+                  <h4>{move.accuracy}</h4>
+                </div>
+              )}
+            </MoveInfo>
+          )}
+          <MoveEffect attack={!!(move.power || move.accuracy)}>
+            {effectEntrie}
+          </MoveEffect>
+        </>
       )}
     </Container>
   );
 };
+
+interface MoveTypeProps {
+  type: string;
+}
+
+function MoveType({ type }: MoveTypeProps) {
+  return (
+    <MoveTypeContainer>
+      <p>{type}</p>
+    </MoveTypeContainer>
+  );
+}
--- src/App/components/PokemonMove/styles.ts
+++ src/App/components/PokemonMove/styles.ts
@@ -1,8 +1,9 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { darken } from "polished";
 
 interface ContainerProps {
   color?: string;
+  open: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -11,18 +12,97 @@ export const Container = styled.div<ContainerProps>`
   background: ${({ theme, color }) =>
     color ? darken(0.05, color) : theme.primary.light};
   border-radius: 8px;
+
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: center;
+  column-gap: 8px;
+  row-gap: 8px;
+  grid-template-rows: 1fr;
+  overflow-y: hidden;
+
+  transition: 0.25s ease-out;
+  max-height: 48px;
+
+  ${({ open }) =>
+    open &&
+    css`
+      transition: 0.25s ease-in;
+      max-height: 200px;
+    `};
+
+  cursor: pointer;
+
   + div {
     margin-top: 8px;
   }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > h4 {
+    margin-right: 8px;
+  }
 
   > div {
-    :nth-of-type(1) {
-      > h4 {
-        margin-right: 8px;
-      }
-      display: flex;
-      align-items: center;
-      margin-bottom: 8px;
+    display: grid;
+    grid-auto-flow: row;
+    row-gap: 8px;
+  }
+`;
+
+export const MoveInfo = styled.div`
+  display: flex;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: ${({ theme }) => theme.primary.light};
+    padding: 8px;
+    border-radius: 16px;
+    width: 60px;
+
+    > p {
+      font-size: 12px;
+    }
+
+    + div {
+      margin-left: 8px;
     }
   }
+`;
+
+interface MoveEffectProps {
+  attack: boolean;
+}
+
+export const MoveEffect = styled.div<MoveEffectProps>`
+  grid-column: 1 / ${({ attack }) => (!attack ? 2 : 3)};
+`;
+
+export const MoveTypeAndBadge = styled.div`
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+`;
+
+export const MoveTypeContainer = styled.div`
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+  align-self: flex-start;
+
+  background: ${({ theme }) => theme.primary.light};
+  color: #fff;
+  font-size: 14px;
+  width: 80px;
+  padding: 8px 16px;
+  border-radius: 32px;
+
+  display: grid;
+  place-content: center;
+  font-weight: 700;
 `;
